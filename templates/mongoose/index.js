c8ca10e7e676b9c3ee728185a7e2c081a08d7eea
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-08-23 20:59:37 +0700

    add templates

--- templates/mongoose/index.js
+++ templates/mongoose/index.js
@@ -1,1 +1,24 @@
-mongoose
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.get("/", (req, res) => {
+   res.json({
+      message: "Hello World!👋",
+   });
+});
+
+// MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+app.use(morgan("dev"));
+
+// PORT
+const port = 3000;
+app.listen(port, () =>
+  console.log(`⦗INFO🔥⦘ Server running on http://localhost:${port}`)
+);
